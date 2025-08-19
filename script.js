4ad6001753290e2cb67033e93b9e39624fec68e9
Hiro16461 <158548582+Hiro16461@users.noreply.github.com>
2025-08-20 08:51:13 +0900

    add form reset function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const projectForm = document.querySelector('.project_form');
 const projectDeleteBtn = document.querySelector('.project-delete');
 const addTask = document.querySelector('.task-plus');
 const addTaskForm = document.querySelector('.task-form');
+const addTaskCancelButton = document.querySelector('.add-task-cancel');
 
 class TodoList {
 	constructor(projectName, description, dueDate, priority) {
@@ -49,9 +50,63 @@ addTask.addEventListener('click', (e) => {
 	addTaskForm.classList.remove('hidden');
 });
 
+addTaskCancelButton.addEventListener('click', (e) => {
+	addTaskForm.classList.add('hidden');
+});
+
+addTaskForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	let taskName = document.querySelector('#task_name').value;
+	let taskDescription = document.querySelector('#task_description').value;
+	let taskDate = document.querySelector('#task_date').value;
+	let taskPriority = document.querySelector('#task_priority').value;
+
+	const newTodoList = new TodoList(
+		taskName,
+		taskDescription,
+		taskDate,
+		taskPriority
+	);
+
+	const taskCheckBox = document.createElement('div');
+	taskCheckBox.classList.add('task-checkBox');
+
+	const taskCheck = document.createElement('input');
+	taskCheck.type = 'checkbox';
+	taskCheck.name = 'task-check';
+	taskCheck.id = 'task-check';
+
+	const taskPara = document.createElement('p');
+	taskPara.textContent = `${newTodoList.projectName}`;
+	taskPara.classList.add('strikethrough');
+
+	taskCheckBox.appendChild(taskCheck);
+	taskCheckBox.appendChild(taskPara);
+
+	const taskCompleted = document.createElement('p');
+	taskCompleted.textContent = 'x';
+	taskCompleted.classList.add('task-completed');
+
+	const tasksName = document.querySelector('.tasks-name');
+
+	const task = document.createElement('div');
+    task.classList.add('task');
+    task.appendChild(taskCheckBox)
+    task.appendChild(taskCompleted)
+ 
+    tasksName.appendChild(task)
+
+	const taskDetail = document.createElement('div');
+    taskDetail.classList.add('task-detail');
+
+    addTaskForm.reset();
+
+	console.log(newTodoList);
+    addTaskForm.classList.add('hidden');
+});
+
 // delete project
 // Already tried↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-
 // projectDeleteBtn.addEventListener('click', (e) => {
 //     const project = document.querySelector('.project');
 //     project.parentNode.removeChild(project)
